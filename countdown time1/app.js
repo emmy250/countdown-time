ac6972ee4ee0ae3f2ac4aecaf31afec3efec090b
emmy250 <95543068+emmy250@users.noreply.github.com>
2022-02-15 12:35:40 +0200

    Update app.js

--- countdown time1/app.js
+++ countdown time1/app.js
@@ -26,9 +26,13 @@ const weekdays = [
   const deadline = document.querySelector(".deadline");
   const items = document.querySelectorAll(".deadline-format  h4");
 
-  let futureDate = new Date(2022,3 ,15,0,0);
-  console.log(futureDate);
+  let tempDate = new Date();
+  let tempyear = tempDate.getFullYear();
+  let tempmonth = tempDate.getMonth();
+  let tempday = tempDate.getDate();
 
+  //let futureDate = new Date(2022,3 ,15,0,0);
+  const futureDate = new Date(tempyear,tempmonth,tempday + 10,11,30,0);
   const year = futureDate.getFullYear();
   const hour = futureDate.getHours();
   const min = futureDate.getMinutes();
